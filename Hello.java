047376820926f9bda71daed1698ce28e7deab18f
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-03-18 11:38:12 +0200

    add arrays

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 class Hello{
 public static void main(String a[]){
 System.out.println("Hello world");
+int [] nums = {1,2,3,4,5};
+int [] nums2 = new int [4];
+System.out.println(nums2[0]);
 }
 }
 
