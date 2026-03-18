daa5c9458ab695b4bf0689fde2fe62cda846a77a
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-03-18 11:44:11 +0200

    add StringBuffer

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 class Hello{
 public static void main(String a[]){
-System.out.println("Hello world");
-int [] nums = {1,2,3,4,5};
-int [] nums2 = new int [4];
-System.out.println(nums2[0]);
+    StringBuffer name = new StringBuffer("Noella");
+    name.charAt(2);
+    name.append(" Muthoni");
+    System.out.println(name.charAt(2));
 }
 }
 
