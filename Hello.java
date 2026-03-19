0d332c888295ebdc7fe9d1deb2ac81d919c38d02
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-03-19 11:30:51 +0200

    add the getters of the private variables

--- Hello.java
+++ Hello.java
@@ -1,10 +1,32 @@
+
+
+
+class Animal{
+    private int age = 10;
+    private String name = "Tommy";
+
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    
+}
 class Hello{
 public static void main(String a[]){
-    StringBuffer name = new StringBuffer("Noella");
-    name.charAt(2);
-    name.append(" Muthoni");
-    System.out.println(name.charAt(2));
+
+    Animal obj = new Animal();
+    // obj.age = 10;
+    // obj.name = "Tommy";
+
+    obj.getAge();
+    obj.getName();
+    
+System.out.println(obj.getAge());
+System.out.println(obj.getName());
 }
+
 }
 
 
