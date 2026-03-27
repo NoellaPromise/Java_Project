0e8937ae992beeb6b8db39867ff53df700fbae90
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-03-27 10:53:58 +0200

    create static variable color

--- Hello.java
+++ Hello.java
@@ -4,19 +4,22 @@
 class Animal{
     int age ;
     String name ;
+   static String color ;
     
-    public Animal(int age , String name){
+    public Animal(int age , String name , String color){
         this.age = age;
         this.name = name;
+        Animal.color = color;
     }
 }
 
 class Hello{
 public static void main(String a[]){
 
-Animal obj = new Animal (10 , "Dog");
+Animal obj = new Animal (10 , "Dog" , "Black");
     System.out.println(obj.age);
     System.out.println(obj.name);
+    System.out.println(obj.color);
 }
 
 }
